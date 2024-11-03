c43eeb9ac8450ef44c5cf70e300f795e15a385d8
Brighter <brighteragyemang@gmail.com>
2024-11-03 16:11:32 +1100

    updated allowed host

--- config/django/prod.py
+++ config/django/prod.py
@@ -1,4 +1,4 @@
 from .base import *  # noqa
 
 DEBUG = False
-ALLOWED_HOSTS = ["127.0.0.1", "0.0.0.0", "localhost", "spotter_routing.onrender.com"]
+ALLOWED_HOSTS = ["127.0.0.1", "0.0.0.0", "localhost", "spotter-routing.onrender.com"]
